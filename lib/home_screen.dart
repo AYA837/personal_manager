0c54325eae03938a5541007cff006c2a15860dba
aya laouadi <aya.laouadi@ensia.edu.dz>
2026-08-12 21:39:51 +0100

    added home screen page

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,14 +13,79 @@ class _HomeScreenState extends State<HomeScreen> {
   //to control the dimension of the stupid element we built
   //everytime we press the button the width and height changes depending on what's inside the on pressed changes
  
+  Widget _WelcomeWidget(){
 
+    return Container(
+        margin: EdgeInsets.only(
+            left: 40,
+            right: 40,
+            bottom:40,
+        ),
+        padding: EdgeInsets.all(20),
+     
+     width: double.infinity,
+     height: 300,
+
+     //always remember the color property shouldn't be the same with the decoration property
+     decoration: _getBoxDecoration(),
+     child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      
+        children: [
+            const SizedBox(height: 10,),
+             Text("Smartly Manage your "
+               ,style: _getTextStyle(),
+             ),
+               Text("Tasks "
+               ,style: _getTextStyle(),
+             ),
+
+             const SizedBox(height: 5,),
+            Text("your ultimate app to organize your personal work"
+              ,style: _getLightTextStyle(),
+            
+            )
+
+        ],
+    )
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Animated Foo Basics') , backgroundColor: Colors.pink,),
+     backgroundColor: Colors.pinkAccent,
       body: Center(
         child: Column(
+           children: [
+              Spacer(flex: 2,),
+              _WelcomeWidget(),
+           ],
+
+
         ))
+    );
+  }
+  TextStyle _getTextStyle(){
+     return const TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+     );
+  }
+
+  BoxDecoration _getBoxDecoration(){
+    return BoxDecoration(
+         borderRadius: BorderRadius.circular(25),
+         color: Colors.white,
+    );
+  }
+  
+  TextStyle _getLightTextStyle(){
+    return TextStyle(
+        fontSize: 15,
+        fontWeight: FontWeight.w200,
+        fontFamily: ,
+        color: const Color.fromARGB(255, 69, 63, 63),
     );
   }
 }
