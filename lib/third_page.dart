f325c7b3e6c7469785e3d4899fe44d91e1f07070
aya laouadi <aya.laouadi@ensia.edu.dz>
2026-08-16 23:17:12 +0100

    modified third page

--- lib/third_page.dart
+++ lib/third_page.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:personal_manager/Widgets/widgets_page.dart';
-//hello world
 
-class ThirdPage extends StatelessWidget {
+// hello world
+
+class ThirdPage extends StatefulWidget {
   const ThirdPage({super.key});
 
- 
+  @override
+  State<ThirdPage> createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  //we will be implmenting buttomnavigation bar feature 
+
+  
+  int selectedIndex = 0;
+  Widget List
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
-        
-        );
-        
-        
-        
-}
-        
-        
-        
+      
+    );
+  }
 }
