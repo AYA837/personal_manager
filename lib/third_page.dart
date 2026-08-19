920260b222bc66c823333158ede81ea616b73fa6
aya laouadi <aya.laouadi@ensia.edu.dz>
2026-08-19 15:47:58 +0100

    modified third page added welcome and date text

--- lib/third_page.dart
+++ lib/third_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:personal_manager/Widgets/widgets_page.dart';
+//import 'package:personal_manager/Widgets/widgets_page.dart';
+import 'package:intl/intl.dart';
 
 // hello world
 
@@ -11,15 +12,53 @@ class ThirdPage extends StatefulWidget {
 }
 
 class _ThirdPageState extends State<ThirdPage> {
-  //we will be implmenting buttomnavigation bar feature 
+  DateTime todayDate = DateTime.now();
 
-  
-  int selectedIndex = 0;
-  Widget List
+  String get formattedDate =>
+      DateFormat(' d MMMM yyyy').format(todayDate);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 40,
+                top: 60,
+              ),
+              child: Text(
+                formattedDate,
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              
+              margin: const EdgeInsets.only(
+                left: 45,
+                top: 10,
+              ),
+              child: const Text(
+                "Welcome Back Aya!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
