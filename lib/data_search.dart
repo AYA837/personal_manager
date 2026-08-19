283d4a319636894ba8feae7f021d15e2bada3d8f
aya laouadi <aya.laouadi@ensia.edu.dz>
2026-08-19 23:17:30 +0100

    added icon setting up , nothing is working yet

--- lib/data_search.dart
+++ lib/data_search.dart
@@ -4,33 +4,48 @@
 
 //and override the 4 commun functions
 
-
 import 'package:flutter/material.dart';
 
-class DataSearch extends SearchDelegate<String>{
+class DataSearch extends SearchDelegate<String> {
+  //the available search options for the user
+  final listOfSearches = ["Tasks", "Projects", "Calender", "Diary", "Notes"];
   @override
   List<Widget>? buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
+    //the search Icon for the bar
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
   }
+
+
+  //the widget below is for icon button  functionality
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    throw UnimplementedError();
+    return IconButton(onPressed: (){
+      Navigator.pop(context);
+    },
+     icon: AnimatedIcon(icon: icon,
+     
+      progress: progress
+      
+      ),
+     
+     );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
     throw UnimplementedError();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
     throw UnimplementedError();
   }
-
-
 }
