9a04bd9163c5f7e877f8524a1becf2cc6f1436c0
aya laouadi <aya.laouadi@ensia.edu.dz>
2026-08-21 16:17:25 +0100

    added new third-page file

---
+++ lib/third_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:personal_manager/Widgets/widgets_page.dart';
+import 'package:intl/intl.dart';
+
+
+// hello world
+
+class ThirdPage extends StatefulWidget {
+  const ThirdPage({super.key});
+
+  @override
+  State<ThirdPage> createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  DateTime todayDate = DateTime.now();
+
+  String get formattedDate =>
+      DateFormat(' d MMMM yyyy').format(todayDate);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+
+        children: [
+           ClipPath(
+
+            clipper: MyWavedClipper(), //IT's defined in the re-usable widgets file
+
+            child: Container(
+              color: Colors.pinkAccent,
+              width: 400,
+              height: 300,
+            ),
+           )
+        ],
+      ),
+      
+      
+      );
+  }
+}
